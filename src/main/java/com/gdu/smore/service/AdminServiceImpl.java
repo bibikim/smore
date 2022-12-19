30e56e1913711eb042b281b9fc5438dcc440386c
sjkh973 <sjkh973@hanmail.net>
2022-12-19 14:32:56 +0900

    js 2022-12-19 다중삭제복구

--- src/main/java/com/gdu/smore/service/AdminServiceImpl.java
+++ src/main/java/com/gdu/smore/service/AdminServiceImpl.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
 
 import com.gdu.smore.domain.user.AllUserDTO;
 import com.gdu.smore.domain.user.RetireUserDTO;
